28a8b02921aa5133e9a557334e6df82c413fd110
Jakub Chmielewski <jakub@jchpro.pl>
2024-01-23 19:27:48 +0100

    My first openai API request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import {configDotenv} from "dotenv";
+import OpenAI from "openai";
 
 configDotenv();
 
-// TODO play!
+const openai = new OpenAI();
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Tell me in one sentence, what is parallax.' }],
+    model: 'gpt-3.5-turbo',
+    n: 1
+  });
+  console.log(chatCompletion);
+  console.log(chatCompletion.choices[0]);
+}
+
+main();
